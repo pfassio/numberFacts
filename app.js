b2950f8bfb5d5d65f0969765d20e3245dd93b778
pfassio <pablofassio@gmail.com>
2018-05-10 17:36:57 +0200

    Commit the app

---
+++ app.js
@@ -0,0 +1,22 @@
+
+  let fact = document.querySelector('#fact');
+  let factText = document.querySelector('#factText');
+
+  let numberInput = document.querySelector('#numberInput');
+  numberInput.addEventListener('input', getFactFetch);
+
+
+  // Fetch with Fetch API
+  function getFactFetch(){
+    let number = numberInput.value;
+
+    if(number != ''){
+      fetch('http://numbersapi.com/'+number)
+      .then(response => response.text())
+      .then(data => {
+        fact.style.display = 'block';
+        factText.innerText = data;
+      })
+      .catch(err => console.log(err));
+    }
+  }
